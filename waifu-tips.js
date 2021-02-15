d63bead7fda1280cd7dee5edf305f6fe6498b056
wuchunlin <1339472610@qq.com>
2021-02-15 19:49:23 +0800

    修改

--- waifu-tips.js
+++ waifu-tips.js
@@ -71,9 +71,6 @@ function loadWidget(config) {
 			Live2D.captureName = "photo.png";
 			Live2D.captureFrame = true;
 		});
-		document.querySelector("#waifu-tool .fa-info-circle").addEventListener("click", () => {
-			open("https://github.com/stevenjoezhang/live2d-widget");
-		});
 		document.querySelector("#waifu-tool .fa-times").addEventListener("click", () => {
 			localStorage.setItem("waifu-display", Date.now());
 			showMessage("愿你有一天能与重要的人重逢。", 2000, 11);
